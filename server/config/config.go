87c738631437151055efa0e3c77542aa4c444a41
magu007 <166204872+magu007@users.noreply.github.com>
2024-05-25 10:27:23 +0800

    update telemetry default config

--- server/config/config.go
+++ server/config/config.go
@@ -303,8 +303,10 @@ func DefaultConfig() *Config {
 			AppDBBackend:        "",
 		},
 		Telemetry: telemetry.Config{
-			Enabled:      false,
-			GlobalLabels: [][]string{},
+			Enabled:                 true,
+			GlobalLabels:            [][]string{},
+			ServiceName:             "cosmostelemetry",
+			PrometheusRetentionTime: 172800,
 		},
 		API: APIConfig{
 			Enable:             false,
